309d94b48b45583832c0604ef8a0ea6b24348be3
Shaon Samanta <156128187+RyukiDoesIt@users.noreply.github.com>
2025-02-02 12:47:39 +0530

    add wallpaper change script

---
+++ .local/share/bin/wallpaper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wallpapers="/home/ryuki/Pictures/Wallpapers"
+ranwall=$(eza $wallpapers | shuf | head -1)
+ranwall="$wallpapers/$ranwall"
+
+swww img $ranwall --transition-type=random
