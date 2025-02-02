d856e9b79acb1cc193c4fe2461c9797c80c00b32
Shaon Samanta <156128187+RyukiDoesIt@users.noreply.github.com>
2025-02-02 12:59:36 +0530

    change transition type

--- .local/share/bin/wallpaper.sh
+++ .local/share/bin/wallpaper.sh
@@ -4,4 +4,4 @@ wallpapers="/home/ryuki/Pictures/Wallpapers"
 ranwall=$(eza $wallpapers | shuf | head -1)
 ranwall="$wallpapers/$ranwall"
 
-swww img $ranwall --transition-type=random
+swww img $ranwall --transition-type=grow
